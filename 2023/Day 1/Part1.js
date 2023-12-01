5062a97b9f007d7410c73aaff577fb5afafabaea
Edward Li <edwardli744@gmail.com>
2023-12-01 09:06:26 -0800

    cleaned

--- 2023/Day 1/Part1.js
+++ 2023/Day 1/Part1.js
@@ -1,12 +1,3 @@
-//The newly-improved calibration document consists of lines of text;
-//each line originally contained a specific calibration value that the Elves now need to recover.
-//On each line, the calibration value can be found by combining the first digit and the last digit (in that order)
-// to form a single two-digit number.
-
-//3six7 => 37 should remove all letters in between
-
-//only first and last digit - pop and shift each index until a number is reached
-
 const fs = require('fs');
 const filePath = './input.txt';
 const inputValues = [];
