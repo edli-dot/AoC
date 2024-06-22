3d99251d769e9b6040cb8c2f68ad20f1d3bf350f
Edward Li <edwardli744@gmail.com>
2024-06-22 13:10:50 -0700

    answer found

--- 2023/day4/part2.js
+++ 2023/day4/part2.js
@@ -14,8 +14,7 @@ fs.readFile(filePath, 'utf-8', async (err, data) => {
     //separates winning numbers : numbers you have parsedData[x][x]
     return pairs.map((pair) => pair.split('| ')); // Split each pair by '| '
   });
-
-  let points = 0;
+  let cardCount = Array(parsedData.length).fill(1);
   for (let i = 0; i < parsedData.length; i++) {
     const wins = parsedData[i][1][0].trim().split(' ');
     const numbersPos = parsedData[i][1][1]
@@ -25,10 +24,15 @@ fs.readFile(filePath, 'utf-8', async (err, data) => {
     let winners = new Set(wins);
     let count = 0;
     for (let j = 0; j < numbersPos.length; j++) {
+      //for each matching number, you get a copy of cards
       if (winners.has(numbersPos[j])) count++;
     }
-    if (count == 1) points += 1;
-    else if (count > 1) points += 2 ** (count - 1);
+    //for each copy of this card, you get additional copies of the subsequent cards
+    while (count > 0) {
+      if (i + count < parsedData.length) cardCount[i + count] += cardCount[i];
+      count--;
+    }
   }
-  console.log(points);
+  const totalCard = cardCount.reduce((x, y) => x + y, 0);
+  console.log(totalCard);
 });
