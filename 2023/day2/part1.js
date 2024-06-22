74c8582da40d3b5ade0bab822ef9b42309785ace
Edward Li <edwardli744@gmail.com>
2024-06-22 01:10:49 -0700

    right answer

--- 2023/day2/part1.js
+++ 2023/day2/part1.js
@@ -7,7 +7,6 @@ fs.readFile(filePath, 'utf-8', async (err, data) => {
     return;
   }
   data = data.split('\n').map((pair) => pair.split(': '));
-
   for (let i = 0; i < data.length; i++) {
     data[i][1] = data[i][1].split(';');
     for (let j = 0; j < data[i][1].length; j++) {
@@ -25,45 +24,45 @@ fs.readFile(filePath, 'utf-8', async (err, data) => {
     blue: 14,
   };
   let gameIDSum = 0;
-  let gameStatus = false;
+  //data [line] => [game 11], [[blue: 1 , red: 1 , green: 1], [blue;1 , red: 1, green: 1]]
   for (let i = 0; i < data.length; i++) {
+    //goes through each line
+    let gameStatus = false;
     for (let j = 0; j < data[i][1].length; j++) {
+      //iterate through each wdra
+      let blueB = 0;
+      let redB = 0;
+      let greenB = 0;
       for (let k = 0; k < data[i][1][j].length; k++) {
-        const color = Object.keys(data[i][1][j][k]);
-        if (validChecks[color[0]] < data[i][1][j][k][color[0]]) {
-          gameIDSum += i;
-          gameStatus = true;
-          //Break out of the innermost loop (k loop)
-          break;
+        //iterate through each color of the draw
+        let color = Object.keys(data[i][1][j][k])[0];
+        // console.log(color, color[0], data[i][1][j].length);
+        switch (color) {
+          case 'blue':
+            blueB += data[i][1][j][k][color];
+            break;
+          case 'red':
+            redB += data[i][1][j][k][color];
+            break;
+          default:
+            greenB += data[i][1][j][k][color];
+            break;
         }
       }
-      if (gameStatus) {
-        //Break out of j loop
-        gameStatus = false;
+      if (
+        blueB > validChecks['blue'] ||
+        redB > validChecks['red'] ||
+        greenB > validChecks['green']
+      ) {
+        //gameIDSum += i;
+        gameStatus = true;
+        //Break out of the innermost loop (k loop)
         break;
       }
     }
-
-    // Continue processing if the condition is not met
+    if (gameStatus == false) gameIDSum += i + 1;
   }
 
-  //   for (let i = 0; i < 1; i++) {
-  //     for (let j = 0; j < 1; j++) {
-  //       for (let k = 0; k < 1; k++) {
-  //         const color = Object.keys(data[i][1][j][k]);
-  //         console.log(color);
-  //         console.log('53,', validChecks[color[0]]);
-  //         console.log('54', data[i][1][j][k][color[0]]);
-  //         // console.log(data[i][1][j]);
-  //         // console.log(validChecks.color);
-  //         // console.log(data[i][1][j][k].color);
-  //         if (validChecks.color < data[i][1][j][k].color) {
-  //           gameIDSum += i;
-  //           break;
-  //         }
-  //       }
-  //     }
-  //   }
   console.log(gameIDSum);
 });
 
